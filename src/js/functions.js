73a78b15a085b23eecae5d8a996018484e8c751d
Diego Calvo Castillo <diego.calvo@enreda.coop>
2024-11-23 17:28:19 +0100

    change scroll behaviour to instantly

--- src/js/functions.js
+++ src/js/functions.js
@@ -15,7 +15,11 @@ export function setEventListener() {
         document.startViewTransition(() => {
           document.querySelector("#content").innerHTML = newContent;
           document.querySelector("head").innerHTML = newHead;
-          document.documentElement.scroll({ top: 0, left: 0, option: "auto" });
+          document.documentElement.scroll({
+            top: 0,
+            left: 0,
+            option: "instantly",
+          });
           console.log("Navigated to " + toUrl.pathname);
           document.dispatchEvent(new Event("changed-view"));
         });
